333b1434a03705bce6a64b5050a50164e122953e
Rikki Sasda <rikkisasda@gmail.com>
2019-03-07 18:35:21 -0600

    Added current date to day view

--- src/components/dayView/DayView.js
+++ src/components/dayView/DayView.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TaskList from './TaskList';
 import NewTask from './NewTask';
+import moment from'moment';
 
 const DayView = ({tasks, addTask}) => {
   return (
     <div>
+      <div>{moment().format('dddd - MMMM Do, YYYY')}</div>
       <NewTask addTask={addTask}/>
       <TaskList tasks={tasks}/>
     </div>
