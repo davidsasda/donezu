78750385ae92d7b76d887d4e2e7448db0e1283e7
Rikki Sasda <rikkisasda@gmail.com>
2019-04-03 21:22:43 -0500

    refractor

--- src/components/dayView/Task.js
+++ src/components/dayView/Task.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Task = ({task}) => {
+  let style = "bg-white hover:bg-sakura-lite border-b border-steel-lite py-3 pl-2";
+
   if (task.status === 'complete') {
-    return (<div className="line-through text-steel bg-white hover:bg-steel-lite border-b border-steel-lite py-3 pl-2">
-    {task.task}
-  </div>)
+    style = 'line-through text-steel bg-white hover:bg-steel-lite border-b border-steel-lite py-3 pl-2'
   }
+  
   return (
-    <div className="bg-white hover:bg-sakura-lite border-b border-steel-lite py-3 pl-2">
+    <div className={style}>
       {task.task}
     </div>
   )
