5d1cdd5308b945ffb9c697d5ebb4a6c89b72a685
Rikki Sasda <rikkisasda@gmail.com>
2019-04-04 00:32:34 -0500

    started week panel

---
+++ src/components/weekPane/CurrentWeek.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Day from './Day';
+import dateFns from 'date-fns';
+
+const CurrentWeek = ({startOfWeek}) => {
+  return (
+    <div className="pb-3">
+      <Day day={dateFns.addDays(startOfWeek, 6)}/>
+      <Day day={dateFns.addDays(startOfWeek, 5)}/>
+      <Day day={dateFns.addDays(startOfWeek, 4)}/>
+      <Day day={dateFns.addDays(startOfWeek, 3)}/>
+      <Day day={dateFns.addDays(startOfWeek, 2)}/>
+      <Day day={dateFns.addDays(startOfWeek, 1)}/>
+      <Day day={startOfWeek}/>
+    </div>
+  )
+}
+
+export default CurrentWeek;
---
+++ src/components/weekPane/Day.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import dateFns from 'date-fns';
+
+const Day = ({day}) => {
+  let style = 'text-steel';
+
+  if (dateFns.isToday(day)) {
+    style = 'text-ake';
+  } else if (dateFns.isFuture(day)){
+    style = 'text-midnight';
+  }
+
+  return (
+    <div className={style + ' pb-2'}>
+      <b className="font-bold">{dateFns.format(day, 'ddd').toUpperCase()}</b>
+      {dateFns.format(day, ' – MMM D, YYYY')}
+    </div>
+  )
+}
+
+export default Day;
